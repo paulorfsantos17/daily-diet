6daf10e89b7ad139e51c8427c15c7bcb6998dcc4
Paulo Santos <paulorfsantos17@gmail.com>
2024-11-19 14:57:42 -0300

    feat: add methods gets in Meal Entity

--- src/domain/meal/enterprise/meal.ts
+++ src/domain/meal/enterprise/meal.ts
@@ -1,15 +1,37 @@
+import type { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { AggregateRoot } from "src/core/entities/aggregate-root";
 
-interface MealProps {
-    name: string;
-    description: string;
-    date: Date;
-    isOnDiet: boolean;
-    userId: string;
+export interface MealProps {
+    name: string
+    description: string
+    date: Date
+    isOnDiet: boolean
+    userId: UniqueEntityId
 }
 
 export class  Meal extends AggregateRoot<MealProps> {
-  static  create(props: MealProps, id?: string) {
+
+  get name(): string {
+    return this.props.name
+  }
+
+  get description(): string {
+    return this.props.description
+  }
+
+  get date(): Date {
+    return this.props.date
+  }
+
+  get isOnDiet(): boolean {
+    return this.props.isOnDiet
+  }
+
+  get userId(): UniqueEntityId {
+    return this.props.userId
+  }
+  
+  static  create(props: MealProps, id?: UniqueEntityId ) {
     const meal = new Meal(props, id)
     return meal
   }
