b88061f6156ef315d6e4c43016ea73787b85f739
Paulo Santos <paulorfsantos17@gmail.com>
2024-11-21 19:41:22 -0300

    feat: add config BcrypterHasher

--- src/infra/cryptography/cryptography.module.ts
+++ src/infra/cryptography/cryptography.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common'
 import { BcryptHasher } from './bcrypt-hasher'
-import { JwtEncrypter } from './jwt-encrypter'
-import { Encrypter } from '@/domain/identity/application/cryptography/encrypter'
+
 import { HashComparer } from '@/domain/identity/application/cryptography/hash-comparer'
 import { HashGenerator } from '@/domain/identity/application/cryptography/hash-generator'
 @Module({
   providers: [
-    { provide: Encrypter, useClass: JwtEncrypter },
     { provide: HashComparer, useClass: BcryptHasher },
     { provide: HashGenerator, useClass: BcryptHasher },
   ],
-  exports: [Encrypter, HashComparer, HashGenerator],
+  exports: [HashComparer, HashGenerator],
 })
 export class CryptographyModule {}
