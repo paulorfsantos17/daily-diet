7c13b8a22e7b0a9db53e3284ce8c8ae2720f029e
Paulo Santos <paulorfsantos17@gmail.com>
2024-11-21 16:34:03 -0300

    feat: add ZodValidationPipe

---
+++ src/infra/http/pipes/zod-validation-pipe.ts
@@ -0,0 +1,23 @@
+import { PipeTransform, BadRequestException, HttpStatus } from '@nestjs/common'
+import { ZodError, ZodSchema } from 'zod'
+import { fromZodError } from 'zod-validation-error'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    try {
+      const parsedValue = this.schema.parse(value)
+      return parsedValue
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          errors: fromZodError(error),
+          message: 'Validation failed.',
+          statusCode: HttpStatus.BAD_REQUEST,
+        })
+      }
+      throw new BadRequestException('Validation failed')
+    }
+  }
+}
